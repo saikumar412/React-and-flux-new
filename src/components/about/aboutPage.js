d538fe6e6aa08a7bc5c89ee43b8243a2e7e63127
saikumar5 <saikumar5588@yahoo.com>
2017-05-31 17:49:46 -0400

    statics

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -3,6 +3,22 @@
 var React = require('react');
 
 var About = React.createClass({
+    statics: {
+        willTransitionTo: function(transition, params, query, callback) {
+            if(!confirm('Do you want to proceed')) {
+                transition.about();
+            } else {
+                callback();
+            }
+        },
+
+         willTransitionFrom: function(transition, component) {
+            if(!confirm('Do you want to leave')) {
+                transition.about();
+            } 
+         }
+    },      
+
     render: function () {
         return (
             <div>
